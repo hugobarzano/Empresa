5f85330863044c8796fcfc1056b553826a1a33d3
hugobarzano <hugobarzano@gmail.com>
2015-11-10 13:24:19 +0100

    modificando base de datos paraheroku

--- EjerciciosIV/settings.py
+++ EjerciciosIV/settings.py
@@ -76,10 +76,20 @@ WSGI_APPLICATION = 'EjerciciosIV.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+ #       'ENGINE': 'django.db.backends.sqlite3',
+  #      'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   # }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd1ea7k9gsqu3j1',
+        'USER': 'xfkguxxjdcmgrp',
+        'PASSWORD': 'cqk8UhZfqmBKystbg39wVZDWyB',
+        'HOST': 'ec2-54-204-6-113.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
@@ -116,3 +126,5 @@ STATICFILES_DIRS = (
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_URL = '/static/'
+
+
