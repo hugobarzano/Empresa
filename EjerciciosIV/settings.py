cc0e6031e4bad4336e49c589c6e838275c616594
hugobarzano <hugobarzano@gmail.com>
2015-11-10 13:51:03 +0100

    modificando setings

--- EjerciciosIV/settings.py
+++ EjerciciosIV/settings.py
@@ -85,8 +85,10 @@ WSGI_APPLICATION = 'EjerciciosIV.wsgi.application'
 DATABASES = {
   	
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgresql',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
     #'heroku': {
      #   'ENGINE': 'django.db.backends.postgresql_psycopg2',
